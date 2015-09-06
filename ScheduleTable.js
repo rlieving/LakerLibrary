5a9a7b3bb273e0f0520052887e1968d5727d54c4
Rob Lieving <rob@lieving.com>
2015-09-06 11:54:41 -0500

    Update connection detection for unique links

--- ScheduleTable.js
+++ ScheduleTable.js
@@ -1,48 +1,48 @@
 "use strict";
 
 $(function () {
-  
+
     var teamCn = function () {
         var current = document.URL.toLowerCase(),
             page = ["freshman", "sophomore", "junior-varsity", "varsity"],
             teamName;
-        
+
         $.each(page, function(idx, team) {
           teamName = team;
           if (current.indexOf(teamName) > 0) { return false; }
         });
-        
+
         return teamName;
     }(),
-    
+
     tblTitle = function() {
-        
+
         var v = {
             "freshman": "Freshman",
             "sophomore": "Sophomore",
             "junior-varsity": "Junior Varsity",
             "varsity": "Varsity"
         };
-        
+
         return v[teamCn];
     }();
-    
+
     // return the row class based on even/odd row and highlight
     function rowClass (rowNum, highlight) {
-            
+
             var c = (rowNum % 2 === 0) ? "even_row" : "odd_row";
             c += highlight ? " highlight" : "";
-            
+
             return { "class" : c };
     }
-    
+
     function ApplyCss() {
         // shortcut to laker styles
         var st = Laker.style,
             center = { "text-align" : "center"},
             hdrCtr = $.extend({}, st.lakerHeader, center),
             datCtr = $.extend({}, st.lakerData, center);
-        
+
         // apply css styles to the table
         $(".laker_title").css(st.lakerTitle);
         $(".laker_table").css(st.lakerTable);
@@ -53,21 +53,21 @@ $(function () {
         $(".even_row").css(st.evenRow);
         $(".highlight").css(st.highlightRow);
     }
-  
-    $.getJSON(Laker.Connection(teamCn, "scores"), 
+
+    $.getJSON(Laker.Connection(teamCn, "scores"),
 
         function (data) {
-          
+
           // return the string that publishes the game result
           var gameResult = function (game) {
             var r = "";
             if (game.result.length > 0) {
-               r = game.result + "&nbsp;" + game.get("lakerscore") + "-" 
+               r = game.result + "&nbsp;" + game.get("lakerscore") + "-"
                 + game.get("opponentscore");
             }
             return r;
           },
-          
+
           // create a new schedule from the json data
           sched = new Laker.Schedule(data),
           // new table object
@@ -76,39 +76,39 @@ $(function () {
           header = tbl.createHeader({}, {"class": "laker_head"}),
           // row object
           row = {};
-          
-          tbl.createTitle("2015 " + tblTitle + " Schedule", 
+
+          tbl.createTitle("2015 " + tblTitle + " Schedule",
             {"class": "laker_title"});
-          
+
           // add header columns
           header.addTd("DATE").addTd("OPPONENT");
           header.addTd("LOCATION").addTd("RESULT");
-          
+
           // iterate through each game on the schedule
           $.each(sched, function(num, game) {
-            
+
             // create a table row with row and td classes
-            row = tbl.createRow(rowClass(num, (game.result === "W")), 
+            row = tbl.createRow(rowClass(num, (game.result === "W")),
               {"class": "laker_data"});
-            
+
             // add table data to the row
             row.addTd(game.gamedate).addTd(game.get("opponent"));
             row.addTd(game.get("gamelocation")).addTd(gameResult(game));
-            
+
             // add the row to the table
             tbl.Rows.add(row);
           });
-          
+
           // write the scores to the page
-          $("#scores").append(tbl.toString());
-          
+          $("#" + teamCn + "-scores").append(tbl.toString());
+
           ApplyCss();
     });
-    
+
     $.getJSON(Laker.Connection(teamCn, "roster"),
-    
+
         function (data) {
-           
+
            var team = new Laker.Team(data),
                tbl = new Laker.html.Table({ "class" : "laker_table"}),
           // new table header object
@@ -117,26 +117,26 @@ $(function () {
           datactr = { "class" : "laker_data_ctr" },
           // row object
           row = {};
-          
+
           tbl.createTitle(tblTitle + " Roster", {"class": "laker_title"});
-          
+
           header.addTd("NAME").addTd("JERSEY", headctr);
           header.addTd("GRADE", headctr).addTd("POSITION", headctr);
-          
+
           $.each(team, function(num, player) {
-              row = tbl.createRow(rowClass(num, player.isCaptain), 
+              row = tbl.createRow(rowClass(num, player.isCaptain),
               {"class" : "laker_data"});
-              
+
               row.addTd(player.fullName).addTd(player.get("number"), datactr);
               row.addTd(player.get("grade"),datactr);
               row.addTd(player.get("position"), datactr);
-              
+
               tbl.Rows.add(row);
           });
-          
-          $("#roster").append(tbl.toString());
-          
+
+          $("#" + teamCn +"-roster").append(tbl.toString());
+
           ApplyCss();
-          
+
         });
 });
