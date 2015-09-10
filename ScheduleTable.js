689662206bf81139dad8e02927ba5486d22e5456
Rob <rob@lieving.com>
2015-09-10 06:05:16 -0500

    Updated spacing in Schedule Table

--- ScheduleTable.js
+++ ScheduleTable.js
@@ -1,161 +1,185 @@
 "use strict";
 
-$(function () {
+$(function() {
 
-    var config = [{
-            "cn" : "varsity",
-            "divSched": "#varsity-scores",
-            "divRoster": "#varsity-roster",
-            "titleSched": "2015 Varsity Schedule",
-            "titleRoster": "2015 Varisty Roster"
-        }, {
-          "cn" : "jv",
-          "divSched": "#jv-scores",
-          "divRoster": "#jv-roster",
-          "titleSched": "2015 Junior Varsity Schedule",
-          "titleRoster": "2015 Junior Varisty Roster",
-        }, {
-          "cn": "sophomore",
-          "divSched": "#sophomore-scores",
-          "divRoster": "#sophomore-roster",
-          "titleSched": "2015 Sophomore Schedule",
-          "titleRoster": "2015 Sophomore Roster"
-        }, {
-          "cn" : "freshman",
-          "divSched": "#freshman-scores",
-          "divRoster": "#freshman-roster",
-          "titleSched": "2015 Freshman Schedule",
-          "titleRoster": "2015 Freshman Roster"
-        }];
+  var config = [{
+    "cn": "varsity",
+    "divSched": "#varsity-scores",
+    "divRoster": "#varsity-roster",
+    "titleSched": "2015 Varsity Schedule",
+    "titleRoster": "2015 Varisty Roster"
+  }, {
+    "cn": "jv",
+    "divSched": "#jv-scores",
+    "divRoster": "#jv-roster",
+    "titleSched": "2015 Junior Varsity Schedule",
+    "titleRoster": "2015 Junior Varisty Roster",
+  }, {
+    "cn": "sophomore",
+    "divSched": "#sophomore-scores",
+    "divRoster": "#sophomore-roster",
+    "titleSched": "2015 Sophomore Schedule",
+    "titleRoster": "2015 Sophomore Roster"
+  }, {
+    "cn": "freshman",
+    "divSched": "#freshman-scores",
+    "divRoster": "#freshman-roster",
+    "titleSched": "2015 Freshman Schedule",
+    "titleRoster": "2015 Freshman Roster"
+  }];
 
-    // return the row class based on even/odd row and highlight
-    function rowClass (rowNum, highlight) {
+  // return the row class based on even/odd row and highlight
+  function rowClass(rowNum, highlight) {
 
-            var c = (rowNum % 2 === 0) ? "even_row" : "odd_row";
-            c += highlight ? " highlight" : "";
+    var c = (rowNum % 2 === 0) ? "even_row" : "odd_row";
+    c += highlight ? " highlight" : "";
 
-            return { "class" : c };
-    }
+    return {
+      "class": c
+    };
+  }
 
-    function applyCss() {
-        // shortcut to laker styles
-        var st = Laker.style,
-            center = { "text-align" : "center"},
-            hdrCtr = $.extend({}, st.lakerHeader, center),
-            datCtr = $.extend({}, st.lakerData, center);
+  function applyCss() {
+    // shortcut to laker styles
+    var st = Laker.style,
+      center = {
+        "text-align": "center"
+      },
+      hdrCtr = $.extend({}, st.lakerHeader, center),
+      datCtr = $.extend({}, st.lakerData, center);
 
-        // apply css styles to the table
-        $(".laker_title").css(st.lakerTitle);
-        $(".laker_table").css(st.lakerTable);
-        $(".laker_head").css(st.lakerHeader);
-        $(".laker_head_ctr").css(hdrCtr);
-        $(".laker_data").css(st.lakerData);
-        $(".laker_data_ctr").css(datCtr);
-        $(".even_row").css(st.evenRow);
-        $(".highlight").css(st.highlightRow);
-    }
+    // apply css styles to the table
+    $(".laker_title").css(st.lakerTitle);
+    $(".laker_table").css(st.lakerTable);
+    $(".laker_head").css(st.lakerHeader);
+    $(".laker_head_ctr").css(hdrCtr);
+    $(".laker_data").css(st.lakerData);
+    $(".laker_data_ctr").css(datCtr);
+    $(".even_row").css(st.evenRow);
+    $(".highlight").css(st.highlightRow);
+  }
 
-    function writeSchedule(teamConfig) {
+  function writeSchedule(teamConfig) {
 
-        $.getJSON(Laker.Connection(teamConfig.cn, "scores"),
+    $.getJSON(Laker.Connection(teamConfig.cn, "scores"),
 
-          function (data) {
+      function(data) {
 
-              // return the string that publishes the game result
-              var gameResult = function (game) {
-                  var r = "";
-                  if (game.result.length > 0) {
-                      r = game.result + "&nbsp;" + game.get("lakerscore");
-                      r += "-" + game.get("opponentscore");
-                  }
-                  return r;
-              },
+        // return the string that publishes the game result
+        var gameResult = function(game) {
+            var r = "";
+            if (game.result.length > 0) {
+              r += game.result === "W" ? "" : "&nbsp;";
+              r += game.result + "&nbsp;" + game.get("lakerscore");
+              r += "-" + game.get("opponentscore");
+            }
+            return r;
+          },
 
-            // create a new schedule from the json data
-            sched = new Laker.Schedule(data),
-            // new table object
-            tbl = new Laker.html.Table({"class": "laker_table"}),
-            // new table header object
-            header = tbl.createHeader({}, {"class": "laker_head"}),
-            // row object
-            row = {};
+          // create a new schedule from the json data
+          sched = new Laker.Schedule(data),
+          // new table object
+          tbl = new Laker.html.Table({
+            "class": "laker_table"
+          }),
+          // new table header object
+          header = tbl.createHeader({}, {
+            "class": "laker_head"
+          }),
+          // row object
+          row = {};
 
-              tbl.createTitle(teamConfig.titleSched, {"class": "laker_title"});
+        tbl.createTitle(teamConfig.titleSched, {
+          "class": "laker_title"
+        });
 
-              // add header columns
-              header.addTd("DATE").addTd("OPPONENT");
-              header.addTd("LOCATION").addTd("RESULT");
+        // add header columns
+        header.addTd("DATE").addTd("OPPONENT");
+        header.addTd("LOCATION").addTd("RESULT");
 
-              // iterate through each game on the schedule
-              $.each(sched, function (num, game) {
+        // iterate through each game on the schedule
+        $.each(sched, function(num, game) {
 
-                  // create a table row with row and td classes
-                  row = tbl.createRow(rowClass(num, (game.result === "W")),
-                    {"class": "laker_data"});
-
-                  // add table data to the row
-                  row.addTd(game.gamedate).addTd(game.get("opponent"));
-                  row.addTd(game.get("gamelocation")).addTd(gameResult(game));
-
-                  // add the row to the table
-                  tbl.Rows.add(row);
-              });
-
-              // write the scores to the page
-              $(teamConfig.divSched).append(tbl.toString());
-
-              applyCss();
+          // create a table row with row and td classes
+          row = tbl.createRow(rowClass(num, (game.result === "W")), {
+            "class": "laker_data"
           });
 
-    }
+          // add table data to the row
+          row.addTd(game.gamedate).addTd(game.get("opponent"));
+          row.addTd(game.get("gamelocation")).addTd(gameResult(game));
 
-    function writeRoster(teamConfig) {
+          // add the row to the table
+          tbl.Rows.add(row);
+        });
 
-      $.getJSON(Laker.Connection(teamConfig.cn, "roster"),
+        // write the scores to the page
+        $(teamConfig.divSched).append(tbl.toString());
 
-          function (data) {
+        applyCss();
+      });
 
-             var team = new Laker.Team(data),
-                 tbl = new Laker.html.Table({ "class" : "laker_table"}),
-            // new table header object
-            header = tbl.createHeader({}, { "class" : "laker_head"}),
-            headctr = { "class" : "laker_head_ctr" },
-            datactr = { "class" : "laker_data_ctr" },
-            // row object
-            row = {};
+  }
 
-            tbl.createTitle(teamConfig.titleRoster, {"class": "laker_title"});
+  function writeRoster(teamConfig) {
 
-            header.addTd("NAME").addTd("JERSEY", headctr);
-            header.addTd("GRADE", headctr).addTd("POSITION", headctr);
+    $.getJSON(Laker.Connection(teamConfig.cn, "roster"),
 
-            $.each(team, function(num, player) {
-                row = tbl.createRow(rowClass(num, player.isCaptain),
-                {"class" : "laker_data"});
+      function(data) {
 
-                row.addTd(player.fullName).addTd(player.get("number"), datactr);
-                row.addTd(player.get("grade"),datactr);
-                row.addTd(player.get("position"), datactr);
+        var team = new Laker.Team(data),
+          tbl = new Laker.html.Table({
+            "class": "laker_table"
+          }),
+          // new table header object
+          header = tbl.createHeader({}, {
+            "class": "laker_head"
+          }),
+          headctr = {
+            "class": "laker_head_ctr"
+          },
+          datactr = {
+            "class": "laker_data_ctr"
+          },
+          // row object
+          row = {};
 
-                tbl.Rows.add(row);
-            });
+        tbl.createTitle(teamConfig.titleRoster, {
+          "class": "laker_title"
+        });
 
-            $(teamConfig.divRoster).append(tbl.toString());
+        header.addTd("NAME").addTd("JERSEY", headctr);
+        header.addTd("GRADE", headctr).addTd("POSITION", headctr);
 
-            applyCss();
-
+        $.each(team, function(num, player) {
+          row = tbl.createRow(rowClass(num, player.isCaptain), {
+            "class": "laker_data"
           });
+
+          row.addTd(player.fullName).addTd(player.get("number"),
+            datactr);
+          row.addTd(player.get("grade"), datactr);
+          row.addTd(player.get("position"), datactr);
+
+          tbl.Rows.add(row);
+        });
+
+        $(teamConfig.divRoster).append(tbl.toString());
+
+        applyCss();
+
+      });
+  }
+
+  $.each(config, function(idx, team) {
+
+    if ($(team.divSched).length) {
+      writeSchedule(team);
     }
 
-    $.each(config, function(idx, team) {
-
-        if ($(team.divSched).length) {
-            writeSchedule(team);
-        }
-
-        if($(team.divRoster).length) {
-            writeRoster(team);
-        }
-    });
+    if ($(team.divRoster).length) {
+      writeRoster(team);
+    }
+  });
 
 });
