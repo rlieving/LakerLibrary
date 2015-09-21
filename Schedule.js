712f622f9a8d315c96d6a73d1645c1eb9906e550
Rob <rob@lieving.com>
2015-09-20 21:48:43 -0500

    Test reverse sort

--- Schedule.js
+++ Schedule.js
@@ -7,55 +7,54 @@
         * losses: returns int of losses
         * ties: returns int of ties
 */
-Laker.Schedule = function (data) {
+Laker.Schedule = function(data) {
 
-    var sched = [];
+  var sched = [];
 
-    sched.wins = 0;
-    sched.losses = 0;
-    sched.ties = 0;
-    sched.goalsFor = 0;
-    sched.goalsAgainst = 0;
+  sched.wins = 0;
+  sched.losses = 0;
+  sched.ties = 0;
+  sched.goalsFor = 0;
+  sched.goalsAgainst = 0;
 
-    function validate(dtString) {
+  function validate(dtString) {
 
-        var d = moment(dtString, "M/D/YYYY");
+    var d = moment(dtString, "M/D/YYYY");
 
-        return d.isValid() ? d : moment(Laker.utility.defaultDate);
+    return d.isValid() ? d : moment(Laker.utility.defaultDate);
+  }
+
+  $.each(data.feed.entry, function(idx, val) {
+
+    sched.push(new Laker.Game(validate(val.title.$t).toDate(), val.content
+      .$t));
+
+    switch (sched[idx].result) {
+
+      case 'W':
+        sched.wins += 1;
+        break;
+
+      case 'L':
+        sched.losses += 1;
+        break;
+
+      case 'T':
+        sched.ties += 1;
+        break;
     }
 
-    function gameSort(g1, g2) {
-
-      return g1.gamedate - g2.gamedate;
+    if (sched[idx].played) {
+      sched.goalsFor += sched[idx].lscore;
+      sched.goalsAgainst += sched[idx].oscore;
     }
 
-    $.each(data.feed.entry, function (idx, val) {
+  });
 
-        sched.push(new Laker.Game(validate(val.title.$t).toDate(), val.content.$t));
+  sched.sort(function(g1, g2) {
+    //return g1.gamedate - g2.gamedate;
+    return g2.gamedate - g1.gamedate;
+  });
 
-        switch (sched[idx].result) {
-
-        case 'W':
-            sched.wins += 1;
-            break;
-
-        case 'L':
-            sched.losses += 1;
-            break;
-
-        case 'T':
-            sched.ties += 1;
-            break;
-        }
-
-        if (sched[idx].played) {
-          sched.goalsFor += sched[idx].lscore;
-          sched.goalsAgainst += sched[idx].oscore;
-        }
-
-    });
-
-    sched.sort(gameSort);
-
-    return sched;
+  return sched;
 };
